931cc2f8f8c7f756023f4227045054329d0e6f49
Peter Molnar <petermolnar@financestack.com>
2022-02-02 17:27:18 +0100

    Initial setup with the data model and the data controller.

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Peter Molnar on 24/01/2022.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
